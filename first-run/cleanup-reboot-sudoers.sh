46bc497d37daab31db24fcdfe4aaacc212fa3c60
Simon Mertins <simon@mertins.net>
2026-01-09 22:13:40 +0100

    big fix

---
+++ first-run/cleanup-reboot-sudoers.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+if sudo test -f /etc/sudoers.d/99-omarchy-installer-reboot; then
+  sudo rm -f /etc/sudoers.d/99-omarchy-installer-reboot
+fi
---
+++ first-run/dns-resolver.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# https://wiki.archlinux.org/title/Systemd-resolved
+echo "Symlink resolved stub-resolv to /etc/resolv.conf"
+
+sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
