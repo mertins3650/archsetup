7da65104cfc73ecc5db8297be566f21d51d98ae8
Simon Mertins <simon@mertins.net>
2026-01-12 11:51:00 +0100

    ssh

---
+++ first-run/ssh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+proceedssh=$(gum choose --limit 1 --height 4 --header "Generate new ssh key" "Yes" "No")
+
+if [ "$proceedssh" = "Yes" ]; then
+    EMAIL=$(gum input --placeholder "Enter your email address" \
+        --prompt "Email address")
+    ssh-keygen -t ed25519 -C "$EMAIL"
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/id_ed25519
+fi
