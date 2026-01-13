b6b0004d1cd77703f55f6dd5d8e79f88b24e621e
Simon Mertins <simon@mertins.net>
2026-01-13 22:28:41 +0100

    fix statusline

--- themes/catppuccin/nvim/colors.lua
+++ themes/catppuccin/nvim/colors.lua
@@ -1,8 +1,13 @@
 function ColorMyPencils()
 	vim.cmd.colorscheme("catppuccin-macchiato")
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	local filename_hl = { bg = "#181825", fg = "#CDD6F5" }
+	local inactive_hl = { bg = "#181825", fg = "#89B4FB" }
+
+	vim.api.nvim_set_hl(0, "MiniStatuslineFilename", filename_hl)
+	vim.api.nvim_set_hl(0, "MiniStatuslineInactive", inactive_hl)
 end
+
 return {
 	"catppuccin/nvim",
 	name = "catppuccin",
