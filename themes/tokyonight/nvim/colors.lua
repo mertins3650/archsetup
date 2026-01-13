28eeff39e80c3eb91fc0e6e7fd91a56c3e4b904c
Simon Mertins <simon@mertins.net>
2026-01-13 13:17:47 +0100

    tokyostart

---
+++ themes/tokyonight/nvim/colors.lua
@@ -0,0 +1,16 @@
+function ColorMyPencils()
+	vim.cmd.colorscheme("tokyonight")
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+return {
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			ColorMyPencils()
+		end,
+	},
+}
